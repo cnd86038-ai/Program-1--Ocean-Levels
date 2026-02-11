59102a8e386db391fb8b320d6420d34669bb83df
cnd86038-ai <cnd86038@email.vccs.edu>
2026-02-11 11:40:22 -0500

    Changed # of years but consecutive not just 5, 7, and 10.

--- Program 1- Ocean Levels/main.cpp
+++ Program 1- Ocean Levels/main.cpp
@@ -4,9 +4,11 @@ using namespace std;
 int main()
 {
 	const double RISE_RATE = 1.5; //millimeters per year
-	int years = 5;
+	double rise5Years = RISE_RATE * 5;
 	cout << "Year\tOcean Level (mm/year)" << endl;
 	double oceanLevel = 0.0;
+// Add a variable to define the number of years to simulate
+const int years = 10;
 	for (int year = 1; year <= years; year++) {
 		oceanLevel += RISE_RATE;
 		cout << year << "\t" << oceanLevel << endl;
