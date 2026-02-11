8286b46bcfbf6e8ce54fecaba584795411aee1e0
cnd86038-ai <cnd86038@email.vccs.edu>
2026-02-11 11:24:16 -0500

    Add project files.

---
+++ Program 1- Ocean Levels/main.cpp
@@ -0,0 +1,9 @@
+SET rate = 1.5 mm / year;
+
+SET rise5 = rate * 5;
+SET rise10 = rate * 7;
+SET rise15 = rate * 10;
+
+DISPLAY rise5 AS 'Rise in 5 years',
+DISPLAY rise7 AS 'Rise in 7 years',
+DISPLAY rise10 AS 'Rise in 10 years',
