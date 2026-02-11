53536a419b36e2353012af6ccaaaf20600a26465
cnd86038-ai <cnd86038@email.vccs.edu>
2026-02-11 11:44:11 -0500

    Added lines 8-9, no change to output.

--- Program 1- Ocean Levels/main.cpp
+++ Program 1- Ocean Levels/main.cpp
@@ -5,6 +5,8 @@ int main()
 {
 	const double RISE_RATE = 1.5; //millimeters per year
 	double rise5Years = RISE_RATE * 5;
+	double rise7Years = RISE_RATE * 7;
+	double rise10Years = RISE_RATE * 10;
 
 	// Optionally, print the result
 	cout << "Ocean will rise " << rise5Years << " millimeters in 5 years." << endl;
